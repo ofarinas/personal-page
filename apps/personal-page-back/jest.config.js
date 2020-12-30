a66ac36fea7955137c09a77ba80e5ed70e86aa13
Osvlado Fernandes <you@example.com>
2020-12-30 10:56:25 +0000

    updated monorepo and added personal-page-back nest project

---
+++ apps/personal-page-back/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'personal-page-back',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/personal-page-back',
+};
--- apps/personal-page-front/src/app/app.component.ts
+++ apps/personal-page-front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'personal-page-root',
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'info-page-front';
 }
