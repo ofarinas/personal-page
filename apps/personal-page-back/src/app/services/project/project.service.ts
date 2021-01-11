9ada3970f9331d2cb9eba645fd9708c01b97152f
Osvlado Fernandes <you@example.com>
2021-01-11 16:27:58 +0100

    (back)added all project

--- apps/personal-page-back/src/app/services/project/project.service.ts
+++ apps/personal-page-back/src/app/services/project/project.service.ts
@@ -5,13 +5,28 @@ import {Project} from "@personal-page/model";
 
 @Injectable()
 export class ProjectService {
-    constructor(@InjectModel('Project') public projectRepository: Model<any>) {
-    }
+  constructor(@InjectModel('Project') public projectRepository: Model<any>) {
+  }
 
-    async add(project?: Project): Promise<void> {
-        const companyName = 'test66';
-        const position = 'title66';
-        const newProject = new this.projectRepository(<Project>{companyName, position});
-        newProject.save();
-    }
+  async add(project?: Project): Promise<void> {
+    const companyName = 'test66';
+    const position = 'title66';
+    const newProject = new this.projectRepository(<Project>{companyName, position});
+    newProject.save();
+  }
+
+  async edit(project?: Project): Promise<void> {
+    const newProject = new this.projectRepository(project);
+    newProject.update();
+  }
+
+  async delete(project?: Project): Promise<void> {
+    const newProject = new this.projectRepository(project);
+    newProject.delete();
+  }
+
+  async getAll(): Promise<any> {
+    return await this.projectRepository.find().exec();
+  }
+
 }
